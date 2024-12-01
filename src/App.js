67bade4aa6cf5ebded674a82bc8360dcc252bcf5
PCalape <philipcalapezz@gmail.com>
2024-12-01 18:27:20 +0800

    adjust values and format code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,57 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './home';
-import Login from './login';
-import './App.css';
-import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home";
+import Login from "./login";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState("")
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     // Fetch the user email and token from local storage
-    const user = JSON.parse(localStorage.getItem("user"))
+    const user = JSON.parse(localStorage.getItem("user"));
 
     // If the token/email does not exist, mark the user as logged out
     if (!user || !user.token) {
-      setLoggedIn(false)
-      return
+      setLoggedIn(false);
+      return;
     }
 
     // If the token exists, verify it with the auth server to see if it is valid
     fetch("http://localhost:3080/verify", {
-            method: "POST",
-            headers: {
-                'jwt-token': user.token
-              }
-        })
-        .then(r => r.json())
-        .then(r => {
-            setLoggedIn('success' === r.message)
-            setEmail(user.email || "")
-        })
-  }, [])
+      method: "POST",
+      headers: {
+        "jwt-token": user.token,
+      },
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        setLoggedIn("success" === r.message);
+        setUsername(user.username || "");
+      });
+  }, []);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} />
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                email={username}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Login setLoggedIn={setLoggedIn} setUsername={setUsername} />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/login.js
+++ src/login.js
@@ -2,123 +2,98 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = (props) => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [emailError, setEmailError] = useState("")
-    const [passwordError, setPasswordError] = useState("")
-    
-    const navigate = useNavigate();
-        
-    const onButtonClick = () => {
+  const [username, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-        // Set initial error values to empty
-        setEmailError("")
-        setPasswordError("")
+  const navigate = useNavigate();
 
-        // Check if the user has entered both fields correctly
-        if ("" === email) {
-            setEmailError("Please enter your email")
-            return
-        }
+  const onButtonClick = () => {
+    // Set initial error values to empty
+    setUsernameError("");
+    setPasswordError("");
 
-        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-            setEmailError("Please enter a valid email")
-            return
-        }
-
-        if ("" === password) {
-            setPasswordError("Please enter a password")
-            return
-        }
-
-        if (password.length < 7) {
-            setPasswordError("The password must be 8 characters or longer")
-            return
-        }
-
-        // Check if email has an account associated with it
-        checkAccountExists(accountExists => {
-            // If yes, log in 
-            if (accountExists)
-                logIn()
-            else
-            // Else, ask user if they want to create a new account and if yes, then log in
-                if (window.confirm("An account does not exist with this email address: " + email + ". Do you want to create a new account?")) {
-                    logIn()
-                }
-        })        
-  
-
+    // Check if the user has entered both fields correctly
+    if ("" === username) {
+      setUsernameError("Please enter your username");
+      return;
     }
 
-    // Call the server API to check if the given email ID already exists
-    const checkAccountExists = (callback) => {
-        fetch("http://localhost:3080/check-account", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify({email})
-        })
-        .then(r => r.json())
-        .then(r => {
-            callback(r?.userExists)
-        })
+    if (!/^[a-zA-Z0-9._-]{6,32}$/.test(username)) {
+      setUsernameError("Please enter a valid username");
+      return;
     }
 
-    // Log in a user using email and password
-    const logIn = () => {
-        fetch("http://localhost:3080/auth", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify({email, password})
-        })
-        .then(r => r.json())
-        .then(r => {
-            if ('success' === r.message) {
-                localStorage.setItem("user", JSON.stringify({email, token: r.token}))
-                props.setLoggedIn(true)
-                props.setEmail(email)
-                navigate("/")
-            } else {
-                window.alert("Wrong email or password")
-            }
-        })
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      return;
     }
 
-    return <div className={"mainContainer"}>
-        <div className={"titleContainer"}>
-            <div>Login</div>
-        </div>
-        <br />
-        <div className={"inputContainer"}>
-            <input
-                value={email}
-                placeholder="Enter your email here"
-                onChange={ev => setEmail(ev.target.value)}
-                className={"inputBox"} />
-            <label className="errorLabel">{emailError}</label>
-        </div>
-        <br />
-        <div className={"inputContainer"}>
-            <input
-                value={password}
-                placeholder="Enter your password here"
-                onChange={ev => setPassword(ev.target.value)}
-                className={"inputBox"} />
-            <label className="errorLabel">{passwordError}</label>
-        </div>
-        <br />
-        <div className={"inputContainer"}>
-            <input
-                className={"inputButton"}
-                type="button"
-                onClick={onButtonClick}
-                value={"Log in"} />
-        </div>
+    logIn();
+  };
+
+  // Log in a user using email and password
+  const logIn = () => {
+    fetch("http://localhost:3080/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: username, password }),
+    })
+      .then((r) => r.json())
+      .then((r) => {
+        if ("success" === r.message) {
+          localStorage.setItem(
+            "user",
+            JSON.stringify({ email: username, token: r.token })
+          );
+          props.setLoggedIn(true);
+          props.setEmail(username);
+          navigate("/");
+        } else {
+          window.alert("Wrong email or password");
+        }
+      });
+  };
+
+  return (
+    <div className={"mainContainer"}>
+      <div className={"titleContainer"}>
+        <div>Login</div>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={username}
+          placeholder="Enter your email here"
+          onChange={(ev) => setEmail(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{usernameError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={"inputBox"}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+      <div className={"inputContainer"}>
+        <input
+          className={"inputButton"}
+          type="button"
+          onClick={onButtonClick}
+          value={"Log in"}
+        />
+      </div>
     </div>
-}
+  );
+};
 
-export default Login
+export default Login;
